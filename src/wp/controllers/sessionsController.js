d4bd91581cc6cc26f62b124a243c54ecc26beae0
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-03-15 14:48:42 +0500

    Fix: Remove license check and clean up sessions controller

--- src/wp/controllers/sessionsController.js
+++ src/wp/controllers/sessionsController.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import queryString from 'query-string'
 import response from './../../response.js'
 import { rmSync, readdir, readFile, readFileSync } from 'fs'
- 
+
 const getxscode = (req, res) => {
       readdir('storage/sessions/md_'+res.locals.sessionId, (err, files) => {
           if (err){
@@ -22,8 +22,8 @@ const getxscode = (req, res) => {
 const status = (req, res) => {
     let message = "Successfully retrieved current status";
     readFile('storage/sessions/md_'+res.locals.sessionId+'/creds.json', function( err, data )
-    { 
-        if(err) 
+    {
+        if(err)
         {
            response(res, 403, true, 'Session not created', { status: "connecting", isSession:false })
         }else{
@@ -49,18 +49,16 @@ const create = async (req, res) => {
     const { id, isLegacy, domain } = req.body
     if (isSessionExists(id)) {
         return response(res, 409, false, 'Session already exists, please use another id.')
-    } 
+    }
     // BYPASSED LICENSE CHECK FOR DEVELOPMENT
     try {
         xswpNodeSocketSet(id, isLegacy, domain, res);
     } catch (error) {
         response(res, 500, false, 'Unable to create QR code: ' + error.message);
-    }  
+    }
 }
 
 const initSystemTerminal = async (req, res = null) => {
-    const { domain } = req.body
-
     // BYPASSED LICENSE CHECK FOR DEVELOPMENT
     try{
         // License check bypassed for development
@@ -86,7 +84,8 @@ const del = async (req, res) => {
 }
 
 const licenseCheck = async(req, id, isLegacy = false, res = null) => {
-    function xsender_0x584a(_0x4bbdb6,_0x3164b3){var _0x1c76a8=xsender_0x1c76();return xsender_0x584a=function(_0x584a95,_0x14dd96){_0x584a95=_0x584a95-0x93;var _0x1558c0=_0x1c76a8[_0x584a95];return _0x1558c0;},xsender_0x584a(_0x4bbdb6,_0x3164b3);}var xsender_0x5d23f6=xsender_0x584a;function xsender_0x1c76(){var _0x326aa9=['3575984TKppoE','4607544DuaIJL','88Yrxilq','14xeNCYQ','https://license.igensolutionsltd.com/wp','Unable\x20to\x20create\x20QR\x20code.\x20ERROR\x20-E1','then','10VzrsQA','1078380dvUVDf','15347vZpbqd','22jYafNn','14462001CzSsmT','1245875dygtmX','application/x-www-form-urlencoded','6eGjidC','stringify','6849976YngSLG','Unable\x20to\x20create\x20QR\x20code.\x20Make\x20sure\x20you\x20are\x20using\x20valid\x20license'];xsender_0x1c76=function(){return _0x326aa9;};return xsender_0x1c76();}(function(_0x347e,_0x2d1633){var _0xe469da=xsender_0x584a,_0x1618f4=_0x347e();while(!![]){try{var _0x35db09=-parseInt(_0xe469da(0x97))/0x1*(parseInt(_0xe469da(0xa2))/0x2)+-parseInt(_0xe469da(0x96))/0x3+-parseInt(_0xe469da(0xa0))/0x4+-parseInt(_0xe469da(0x9a))/0x5*(-parseInt(_0xe469da(0x9c))/0x6)+-parseInt(_0xe469da(0xa3))/0x7*(-parseInt(_0xe469da(0x9e))/0x8)+parseInt(_0xe469da(0x99))/0x9*(parseInt(_0xe469da(0x95))/0xa)+parseInt(_0xe469da(0x98))/0xb*(-parseInt(_0xe469da(0xa1))/0xc);if(_0x35db09===_0x2d1633)break;else _0x1618f4['push'](_0x1618f4['shift']());}catch(_0x4b106a){_0x1618f4['push'](_0x1618f4['shift']());}}}(xsender_0x1c76,0xd4de6),axios['post'](xsender_0x5d23f6(0xa4),queryString[xsender_0x5d23f6(0x9d)]({'domain_check':req}),{'headers':{'Content-Type':xsender_0x5d23f6(0x9b)}})[xsender_0x5d23f6(0x94)](function(_0x2856fd){var _0x27f641=xsender_0x5d23f6;if(_0x2856fd['status']==0xc8)try{createSession(id,isLegacy,res);}catch{_0x2856fd(res,0xc8,![],_0x27f641(0x93));}else _0x2856fd(res,0xc8,![],_0x27f641(0x9f));}));
+    // This function is kept for compatibility but does nothing
+    return true;
 }
 
 export { getxscode, status, create, del, licenseCheck, initSystemTerminal }
